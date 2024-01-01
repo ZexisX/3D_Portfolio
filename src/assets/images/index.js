16957e6faf95cdad6cd6fc18711120cd04d7fe59
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2024-01-01 16:41:03 +0700

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,3 @@
-import hero from './hero.jpg'
 import meta from './meta.png'
 import shopify from './shopify.png'
 import starbucks from './starbucks.png'
@@ -6,7 +5,6 @@ import tesla from './tesla.png'
 import logo from './logo.svg'
 
 export {
-    hero,
     meta,
     shopify,
     starbucks,
